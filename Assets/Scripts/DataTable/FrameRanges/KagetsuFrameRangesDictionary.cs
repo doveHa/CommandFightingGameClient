04b1ff578a59bae1a1ff57363ef9fcd4cebf3136
ytdftdtd <111149366+ytdftdtd@users.noreply.github.com>
2025-06-11 05:57:51 +0900

    제이슨데이터
    업데이트

--- Assets/Scripts/DataTable/FrameRanges/KagetsuFrameRangesDictionary.cs
+++ Assets/Scripts/DataTable/FrameRanges/KagetsuFrameRangesDictionary.cs
@@ -4,21 +4,21 @@
     {
         public KagetsuFrameRangesDictionary() : base()
         {
-            AddRange("Idle", new int[] { 31, 62, 91, 121, 122 });
-            AddRange("Walk", new int[] { 11, 21, 31, 41, 42 });
-            AddRange("Jumping_Attack", new int[] { 7, 13, 19, 20 });
-            AddRange("Jumping_Down", new int[] { 1, 2 });
-            AddRange("Jumping_Up", new int[] { 2, 3, 4 });
+            AddRange("Idle", new int[] { 41, 81, 121, 122 });
+            AddRange("Walk", new int[] { 8, 16, 25, 34, 35 });
+            AddRange("Jumping_Attack", new int[] { 6, 10, 13, 18, 24, 27, 31 });
+            AddRange("Jumping_Down", new int[] { 4, 7, 8 });
+            AddRange("Jumping_Up", new int[] { 5, 8, 12, 13 });
             AddRange("Guard", new int[] { 1, 1 });
-            AddRange("Hit", new int[] { 6, 9, 11 });
+            AddRange("Hit", new int[] { 13, 25, 26 });
             AddRange("Airborne", new int[] { 0, 0, 0, 0 });
-            AddRange("Atk_Punch", new int[] { 4, 19, 27, 28 });
-            AddRange("Atk_Kick", new int[] { 7, 14, 20, 29, 36, 47, 47 });
+            AddRange("Atk_Punch", new int[] { 3, 5, 8, 20, 22 });
+            AddRange("Atk_Kick", new int[] { 5, 11, 15, 20, 26, 32, 40 });
             
-            AddRange("IttoRyotan", new int[] { 9, 15, 21, 22 });
-            AddRange("NageKunai", new int[] { 8, 14, 19, 25 });
-            AddRange("Nageru", new int[] { 4, 7, 16, 17 });
-            AddRange("Sangiri", new int[] { 11, 21, 77, 78 });
+            AddRange("IttoRyotan", new int[] { 6, 10, 16, 22, 28, 33, 38, 41, 45, 49 });
+            AddRange("NageKunai", new int[] { 6, 10, 17, 22, 23 });
+            AddRange("Nageru", new int[] { 8, 16, 21, 29, 37, 43 });
+            AddRange("Sangiri", new int[] { 3, 10, 15, 21, 26, 31, 36, 42, 48, 53, 56, 62 });
         }
     }
 }
